77a29a3f3b1911922adb134a766a19ba55ede2af
Érica <ceciliatoerica@gmail.com>
2018-04-05 20:48:19 -0300

    vai caralho

--- app/torii-providers/jwt.js
+++ app/torii-providers/jwt.js
@@ -6,6 +6,7 @@ export default Ember.Object.extend({
         url: 'http://private-8ab80-incube.apiary-mock.com/api/usuario/login',
         data: { email: "email", password: "password" },
         dataType: 'json',
+        method: 'post',
         success: function(result) {
           console.log(result)
           resolve
