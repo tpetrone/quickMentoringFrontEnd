d269ee491eea83d368c8d383a4b73629e0c72063
Petrone <thiago.petrone@gmail.com>
2018-04-06 19:17:26 -0300

    Mostra aplicações

--- app/routes/dashboard/mentoria.js
+++ app/routes/dashboard/mentoria.js
@@ -1,10 +1,13 @@
 import Route from '@ember/routing/route';
+import RSVP from 'rsvp';
 
 export default Route.extend({
   ajax: Ember.inject.service(),
   
   model(params) {
     const mentoria = this.get('ajax').request(`/mentoria/${params.mentoria_id}`, { dataType: 'json' });
-    return mentoria;
+    const aplicacoes = this.get('ajax').request(`mentoria/${params.mentoria_id}/aplicacoes`, { dataType: 'json' });
+
+    return RSVP.hash({mentoria, aplicacoes})
   }
 });
